9501cbb1b102158cb417bb004489e7d0fe0219a9
Dexter732 <dgornick@stmesc.org>
2024-10-30 20:26:38 +0000

    good

--- lessons/00_Turtles/04c_Turtle_Tricks.py
+++ lessons/00_Turtles/04c_Turtle_Tricks.py
@@ -20,11 +20,40 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 
 
 ... # Your code here
-sides=20
-angle=360/sides
-for i in range(sides):
-    tina.forward(40)
-    tina.left(angle)
+tina.circle(30,180)
+tina.forward(70)
+tina.circle(30,180)
+tina.forward(70)
+tina.penup()
+tina.forward(70)
+tina.pendown()
+tina.left(90)
+tina.forward(60)
+tina.circle(75,180)
+tina.penup()
+tina.forward(60)
+tina.pendown()
+tina.forward(140)
+tina.circle(27,180)
+tina.forward(50)
+tina.circle(-21,180)
+tina.forward(50)
+tina.circle(27,180)
+tina.forward(200)
+tina.right(90)
+tina.circle(-40,90)
+tina.forward(70)
+tina.circle(-40,90)
+tina.penup()
+tina.forward(80)
+tina.right(90)
+tina.forward(150)
+tina.right(180)
+tina.pendown()
+tina.circle(-30,90)
+tina.forward(60)
+
 
 
 turtle.exitonclick()
+
--- lessons/00_Turtles/08b_More_Turtle_programs.py
+++ lessons/00_Turtles/08b_More_Turtle_programs.py
@@ -7,4 +7,25 @@ from the section "Change the Turtle Image"
 Then change the code so that the turtle has a different image ( look in the 'images'
 directory ) and moves to the corners of the screen in a square pattern. 
 """
+import turtle
 
+
+def set_turtle_image (turtle,image_name):
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)  
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+
+t = turtle.Turtle()
+
+set_turtle_image(t, "meowscles-fortnite.gif")
+
+
+
+turtle.exitonclick()     
