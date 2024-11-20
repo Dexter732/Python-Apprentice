ac050aaef0d392fc5448c598ba6fb8dd21275923
Dexter732 <dgornick@stmesc.org>
2024-11-20 21:19:16 +0000

    turtle

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -16,5 +16,50 @@ screen.setup(width=600, height=600)
 screen.bgcolor('white')
 
 t = turtle.Turtle()
+t.speed(10)
+turtle.forward(90)
+turtle.forward(-90)
+turtle.forward(90)
+turtle.forward(-90)
+turtle.forward(90)
+turtle.forward(-90)
+turtle.forward(90)
+turtle.forward(-90)
+turtle.forward(90)
+turtle.forward(-90)
 
-... # Your Code Here
+t1 = turtle.Turtle()
+t1.pendown()
+t1.shape("triangle")
+
+t2 = turtle.Turtle()
+t2.pendown()
+t2.shape("circle")
+
+t3 = turtle.Turtle()
+t3.pendown()
+t3.shape("square")
+
+t4 = turtle.Turtle()
+t4.pendown()
+t4.shape("arrow")
+
+t5 = turtle.Turtle()
+t5.pendown()
+t5.shape("square")
+
+
+
+for i in range(-100, 100):
+    t1.goto(40,20)
+    t2.goto(60,-80)
+    t3.goto(60,70)
+    t4.goto(70,20)
+    t5.goto(90,-100)
+t2.forward(40)
+t1.backward(60)
+t3.forward(60)
+t4.left(50)
+t5.forward(40)
+t4.forward(60)
+turtle.exitonclick() 
